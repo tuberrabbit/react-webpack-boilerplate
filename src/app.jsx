a389c77d85d2ebac176eee7d7a4bc32501f6214e
tuber <btu@thoughtworks.com>
2016-11-29 09:08:31 +0800

    [tuber] update app to display my react boilerplate table.

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,24 @@
 import React from 'react';
 
 const App = ()=>(
-    <div>Hello World</div>
+	<table>
+		<caption>My react boilerplate table</caption>
+		<tr>
+			<th>Dependency</th>
+			<th>Version</th>
+		</tr>
+		<tr>
+			<td>react</td>
+			<td>15</td>
+		</tr>
+        <tr>
+            <td>react-redux</td>
+            <td>4</td>
+        </tr>
+        <tr>
+            <td>react-router</td>
+            <td>3</td>
+        </tr>
+	</table>
 );
 export default App;
