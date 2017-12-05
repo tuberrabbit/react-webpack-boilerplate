9b01402cd7d6286b8be4d7043ff337b719d9d77c
tuber <btu@nocode.com>
2017-12-05 12:40:07 +0800

    [tuber] reset user agent stylesheet

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { Route, Router } from 'dva/router';
 import * as React from 'react';
-
 import App from './containers/App';
+import './styles/reset.less';
 
 export default ({ history }) => (
   <Router history={history}>
