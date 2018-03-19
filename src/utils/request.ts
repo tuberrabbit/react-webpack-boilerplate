2df533cd6d137332910bc4c26f6fea0e4f7436cd
tuber <btu@nocode.com>
2018-03-19 12:24:31 +0800

    [tuber] update request config

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,18 +1,29 @@
 import fetch from 'dva/fetch';
 import config from '../config';
 
-const checkStatus = res => {
+const checkStatus = async res => {
   if (res.ok) {
     return res;
   }
   const error: any = new Error(res.statusText);
-  error.response = res;
+  error.body = await res.json();
   throw error;
 };
 
 const request = async (url, opts = {}) => {
-  const res = await fetch(`${config.host}${url}`, opts);
-  checkStatus(res);
+  const defaultOptions = {};
+  const newOptions: any = { ...defaultOptions, ...opts };
+  if (newOptions.method === 'POST' || newOptions.method === 'PUT' || newOptions.method === 'PATCH') {
+    newOptions.headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json; charset=utf-8',
+      ...newOptions.headers,
+    };
+    newOptions.body = JSON.stringify(newOptions.body);
+  }
+
+  const res = await fetch(`${config.host}${url}`, newOptions);
+  await checkStatus(res);
   return res.json();
 };
 
